8453a64fb8b80358a96a5d43752fd3e9e4905f79
Naheed Awan <nadsawan@yahoo.com>
2022-01-04 21:37:32 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+let h1 = document.createElement('h1')
+h1.innerHTML = "WhirlWind Waranty Registration Page"
+document.body.appendChild(h1)
+
+let h2 = document.createElement("h2")
+h2.innerHTML = "Warranty Information"
+document.body.appendChild(h2)
+
+let firstname = document.createElement('p')
+firstname.innerHTML = "First Name"
+document.body.appendChild(firstname)
+let input = document.createElement('input')
+// input.innerHTML="first name"
+document.body.appendChild(input)
+
+let lastname = document.createElement('p')
+lastname.innerHTML = "Last Name"
+document.body.appendChild(lastname)
+let otherinput = document.createElement('input')
+otherinput.innerHTML = "last name"
+document.body.appendChild(otherinput)
+
+let email = document.createElement('p')
+email.innerHTML = "Email"
+document.body.appendChild(email)
+let thirdinput = document.createElement('input')
+// otherinput.innerHTML = "last name"
+document.body.appendChild(thirdinput)
+
+let phoneNumber = document.createElement('p')
+phoneNumber.innerHTML = "Phone Number"
+document.body.appendChild(phoneNumber)
+let fourthinput = document.createElement('input')
+// otherinput.innerHTML = "last name"
+document.body.appendChild(fourthinput)
+
+let address = document.createElement('p')
+address.innerHTML = "Address"
+document.body.appendChild(address)
+let fifthinput = document.createElement('input')
+// otherinput.innerHTML = "last name"
+document.body.appendChild(fifthinput)
+
+let city = document.createElement('p')
+city.innerHTML = "City"
+document.body.appendChild(city)
+let sixthinput = document.createElement('input')
+document.body.appendChild(sixthinput)
+
+// let city = document.createElement('p')
+// city.innerHTML = "City"
+// document.body.appendChild(city)
+// let sixthinput = document.createElement('input')
+// document.body.appendChild(sixthinput)
+
+
+
+
+
+
+
+
+
+
